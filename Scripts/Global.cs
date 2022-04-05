3105c50459b1cc844adb7c52f1eb1967893e7fb2
Archie Baldry <archiebaldry@protonmail.com>
2022-04-05 18:16:00 +0100

    Use colour constant in Global.cs

--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -5,7 +5,7 @@ public class Global : Node
 {
     // Options
     public static string Username = "Player";
-    public static Color Colour = new Color(255F, 0F, 0F);
+    public static Color Colour = Colors.Red;
     public static Vector2 Sensitivity = new Vector2(0.2F, 0.2F);
     public static Vector2 Fov = new Vector2(90, 90);
     
